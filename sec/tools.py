eb9bab9f80eb512f67c9bdc0f679c972a6d3f9cc
raimanta215 <r.macedon@students.ephec.be>
2026-04-06 01:09:20 +0200

    symmetric module mis en place
    chiffremment et dechiffremment AES

--- sec/tools.py
+++ sec/tools.py
@@ -16,12 +16,4 @@ def derive_symmetric_key(shared_secret, info=b'key', length=16):
     aes_key = hkdf_key.derive(shared_secret)
     log.info(f"Derived symmetric key: {safe_key_hash(aes_key)} )length: {len(aes_key)} bytes")
 
-    hkdf_iv = HKDF(
-        algorithm=hashes.SHA256(),
-        length=16, #16 for AES-GCM
-        salt=None,
-        info=b'iv',
-    )
-    aes_iv = hkdf_iv.derive(shared_secret)
-    log.info(f"Derived symmetric IV: {safe_key_hash(aes_iv)} length: {len(aes_iv)} bytes")
-    return aes_key, aes_iv
+    return aes_key
