531233eefdf8aefcd7783b5a0a1a50ec7fb8773f
raimanta215 <r.macedon@students.ephec.be>
2026-04-06 18:54:54 +0200

    Dilithium key gen

--- sec/identity.py
+++ sec/identity.py
@@ -1,9 +1,17 @@
-from sec import SigModule
+from sec import SignModule
+import logging
+
+log = logging.getLogger(__name__)
 
 PATH_TO_CERTS = "certs/"
 SK_NAME = "secret_dil_key.bin"
 PK_NAME = "public_dil_key.bin"
 
 
-sig = SigModule('Dilithium2')
-sig.generate_keypair(PATH_TO_CERTS + PK_NAME, PATH_TO_CERTS + SK_NAME)
+def create_dilithium_key():
+    log.info("Starting certificate generation for SERVER..")
+
+    sig = SignModule('Dilithium2')
+    sig.generate_keypair(PATH_TO_CERTS + PK_NAME, PATH_TO_CERTS + SK_NAME)
+
+
