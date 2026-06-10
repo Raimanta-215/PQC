00f87e1a9e13c1db1b0552fa1ef1a9e739448545
Raimanta-215 <r.macedon@students.ephec.be>
2026-06-10 16:50:51 +0200

    Disable file logging in setup_logger function
    Commented out filename and filemode options for log storage.

--- logger/logger.py
+++ logger/logger.py
@@ -13,14 +13,14 @@ class Logger(logging.Handler):
                 self.app.call_from_thread(self.app.log_to_monitor, log_entry)
         except Exception as e:
             print(f"Error emitting log message: {str(e)}")
-
+# to enable logs storage into file, remove comment lines 'filename' and 'filemode'
 def setup_logger(role=None):
     logging.basicConfig(
         level=logging.INFO,
         format=f'%(asctime)s - [{role}] - %(name)s - %(levelname)s - %(message)s',
         datefmt='%H:%M:%S',
-        filename=f"{role.lower()}.log" if role else 'app.log',
-        filemode='a',
+        #filename=f"{role.lower()}.log" if role else 'app.log',
+        #filemode='a',
         encoding='utf-8',
         force=True
     )
